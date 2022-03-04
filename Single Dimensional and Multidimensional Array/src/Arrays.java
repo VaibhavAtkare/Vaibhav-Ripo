db491f06f3addd42f823e3620cf35419aa167190
VaibhavAtkare <100461487+VaibhavAtkare@users.noreply.github.com>
2022-03-04 21:57:58 +0530

    Add files via upload

---
+++ Single Dimensional and Multidimensional Array/src/Arrays.java
@@ -0,0 +1,11 @@
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// Declare and initialize an array of five integer values.
+	     int[ ] num = {2, 4, 6, 8, 10, 12, 14};
+	  
+	// Display the length of array.
+	     System.out.println("Length of array: " +num.length);
+	}
+}
